6b592d0258110ea5730cd32acf1da727e38a5255
Kasper Rosenkrands <krosenkrands7@gmail.com>
2024-07-07 12:53:39 +0200

    move layout to layout file

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,3 +1,6 @@
+import { Box, Button, Paper, Stack, Typography } from "@mui/material"
+import Link from "next/link"
+
 export const metadata = {
   title: 'Madplanlægger',
 }
@@ -9,7 +12,33 @@ export default function RootLayout({
 }) {
   return (
     <html lang="da">
-      <body>{children}</body>
+      <body>
+        <main>
+          <Box padding={"10px"}>
+            <Box paddingBottom={"10px"}>
+              <Paper elevation={5}>
+                <Box padding="10px">
+                  <Stack direction="row" alignItems="center" justifyContent={"space-between"}>
+                    <Typography variant="h2">Madplanlægger</Typography>
+                  </Stack>
+                  <Paper elevation={5}>
+                    <Box padding="10px">
+                      <Stack direction="row" alignItems="center" justifyContent={"space-evenly"}>
+                        <Link href="/">Hjem</Link>
+                        <Link href="/recipes">Opskrifter</Link>
+                        <Link href="/grocieries">Indkøbsliste</Link>
+                        <Link href="/indgredients">Ingredienser</Link>
+                        <Link href="/uits">Enheder</Link>
+                      </Stack>
+                    </Box>
+                  </Paper>
+                </Box>
+              </Paper>
+            </Box>
+            {children}
+          </Box>
+        </main>
+      </body>
     </html>
   )
 }
---
+++ frontend/src/app/recipes/page.tsx
@@ -0,0 +1,9 @@
+import { Paper, Box, Stack, Typography } from "@mui/material";
+
+export default function Recipes() {
+    return <Paper elevation={5}>
+        <Typography>
+            Dette er opskrifter
+        </Typography>
+    </Paper>
+}
